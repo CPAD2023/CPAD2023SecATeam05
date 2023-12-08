f17632bfbcbb05e5eead358b6968faffe1934831
vish25o <119324105+vish25o@users.noreply.github.com>
2023-12-08 21:22:08 +0530

    Create store.ts

---
+++ src/store/store.ts
@@ -0,0 +1,13 @@
+import {configureStore} from '@reduxjs/toolkit';
+import designReducer from './slices/design.reducer';
+import gameReducer from './slices/game.reducer';
+
+export const store = configureStore({
+  reducer: {
+    game: gameReducer,
+    design: designReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
