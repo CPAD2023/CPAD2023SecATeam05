080dd8b7385d882dd066cb291c971ca667a97cb1
vish25o <119324105+vish25o@users.noreply.github.com>
2023-12-08 21:33:01 +0530

    Create route.type.ts

---
+++ src/hooks/routes/types/route.type.ts
@@ -0,0 +1,9 @@
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+
+export type NavStackParamsType = {
+  Menu: undefined;
+  Play: undefined;
+  Options: undefined;
+};
+
+export type NavStackProps = NativeStackScreenProps<NavStackParamsType>;
