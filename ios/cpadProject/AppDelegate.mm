d8adbee37bf360a26886c8ed3d47ac5321861463
Ayush Kumar <2022sp93024@wilp.bits-pilani.ac.in>
2023-12-09 08:00:27 +0530

    Commit5

--- ios/cpadProject/AppDelegate.mm
+++ ios/cpadProject/AppDelegate.mm
@@ -20,6 +20,6 @@
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
 }
-// You can add your custom initial props in the dictionary below.
+// You can add your custom initial props in the dictionary.
 
 @end
